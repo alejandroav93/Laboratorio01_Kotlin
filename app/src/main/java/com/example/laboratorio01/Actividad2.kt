c92ebb96d31cdb175db0a9ea68b8a1cc35143d5d
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-01-28 13:52:04 -0600

    Text View 2 + Linear Layouts

--- app/src/main/java/com/example/laboratorio01/Actividad2.kt
+++ app/src/main/java/com/example/laboratorio01/Actividad2.kt
@@ -11,13 +11,13 @@ class Actividad2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad2)
 
-        val bundle = intent.extras
+     //   val bundle = intent.extras
 
-        if(bundle != null){
-            val valueA: String = bundle.getString("ValueA", "")
-            Toast.makeText(this, valueA, Toast.LENGTH_SHORT).show()
-            val txtValue: TextView = findViewById(R.id.txtValue)
-            txtValue.text = valueA
-        }
+      //  if(bundle != null){
+         //   val valueA: String = bundle.getString("ValueA", "")
+         //   Toast.makeText(this, valueA, Toast.LENGTH_SHORT).show()
+           // val txtValue: TextView = findViewById(R.id.txtValue)
+          //  txtValue.text = valueA
+       // }
     }
 }
